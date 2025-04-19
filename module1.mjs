78562415dd2634d9f7bebdf2d6f53621316bffd6
Keppiekip <maharjanitika@gmail.com>
2025-04-19 21:29:48 +0545

    how to import and export

---
+++ module1.mjs
@@ -0,0 +1,9 @@
+const a="Luffy";
+const b="Zoro";
+const c="Nami";
+const d="Usopp";
+
+export default c;
+export {a};
+export {b};
+export {d};
---
+++ module2.mjs
@@ -0,0 +1,5 @@
+import first, {a,b,d} from './module1.mjs'
+console.log(first);
+console.log(a);
+console.log(b);
+console.log(d);
